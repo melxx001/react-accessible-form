5589e6c730ed462b507562d2aed641041825b8fa
hichamh <hicham.elhammouchi@concur.com>
2015-06-08 14:53:54 -0400

    Fixed Index page

--- example/views/Index.jsx
+++ example/views/Index.jsx
@@ -5,7 +5,7 @@ var App = require('../build/components/Index')
 var Index = React.createClass({
   render: function() {
     return (
-		<DefaultLayout content={this.props.content} client="AppClient" title={this.props.title}> 
+		<DefaultLayout content={this.props.content} client="IndexClient" title={this.props.title}> 
 			<App />
 		</DefaultLayout>
     )
